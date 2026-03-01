10e791daed362bcffa6c8d7c007ae2b39a9143f4
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-02-28 19:26:55 -0500

    Fix config loading.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,9 +1,14 @@
 #include <QApplication>
+#include <QDir>
 #include "EGIAmpWindow.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    EGIAmpWindow window(nullptr, argc > 1 ? argv[1] : "ampserver_config.cfg");
+    std::string configPath = argc > 1
+        ? argv[1]
+        : QDir(QCoreApplication::applicationDirPath())
+              .filePath("ampserver_config.cfg").toStdString();
+    EGIAmpWindow window(nullptr, configPath);
     window.show();
     return app.exec();
 }
